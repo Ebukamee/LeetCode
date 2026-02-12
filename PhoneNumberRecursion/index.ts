a4144d428c67ae2d32bfb1046a82111bf3b50826
Ebuka <chukwuebukanwokike2007@gmail.com>
2026-02-12 11:56:01 -0700

    TS code implementation of Phone Number combination

--- PhoneNumberRecursion/index.ts
+++ PhoneNumberRecursion/index.ts
@@ -1,3 +1,31 @@
 function letterCombinations(digits: string): string[] {
-    
+    if (digits.length === 0) return []
+
+    const map : Record<string,string> = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    }
+    const result : string[] = []
+
+    function RecursiveBuild(i:number,current:string) {
+
+        if(i==digits.length) {
+            result.push(current)
+            return;
+        }
+           const letters = map[digits[i]];
+
+        for (const letter of letters) {
+            RecursiveBuild(i + 1, current + letter);
+        }
+
+    }
+    RecursiveBuild(0,'')
+    return result
 };
