f894103236b22ec0f1647f3ca1c4f9f0212f1f57
Ebuka <chukwuebukanwokike2007@gmail.com>
2026-02-14 11:40:26 -0700

    TS divide int

--- DivideIntegers/index.ts
+++ DivideIntegers/index.ts
@@ -1,3 +1,30 @@
-func divide(dividend int, divisor int) int {
-    
+function divide(dividend: number, divisor: number): number {
+    const INT_MIN = -2147483648;
+    const INT_MAX = 2147483647;
+
+    if (dividend === INT_MIN && divisor === -1) {
+        return INT_MAX;
+    }
+
+    const negative = (dividend < 0) !== (divisor < 0);
+
+    let a = Math.abs(dividend);
+    let b = Math.abs(divisor);
+    let result = 0;
+
+    while (a >= b) {
+        let temp = b;
+        let multiple = 1;
+
+ 
+        while (a >= temp + temp) {
+            temp = temp + temp;
+            multiple = multiple + multiple;
+        }
+
+        a -= temp;
+        result += multiple;
+    }
+
+    return negative ? -result : result;
 }
