0dd00eae1e8aa0b242cd88a2403c1a0172b50573
Ebuka <chukwuebukanwokike2007@gmail.com>
2026-02-16 10:40:17 -0700

    Search sorted array

---
+++ SearchINRotatedSortedArray/main.go
@@ -0,0 +1,28 @@
+package searchinrotatedsortedarray
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := (left + right) / 2
+
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] >= nums[left] {
+			if target >= nums[left] && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target <= nums[right] && target > nums[mid] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
