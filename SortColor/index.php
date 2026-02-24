5a93e6e8fe393e638efe630fb2d20b366df65cf4
Ebuka <chukwuebukanwokike2007@gmail.com>
2026-02-24 15:25:28 -0700

    sort color

---
+++ SortColor/index.php
@@ -0,0 +1,26 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return NULL
+     */
+    function sortColors(&$nums) {
+        $low = 0;
+        $mid = 0;
+        $high = count($nums) - 1;
+
+    while ($mid <= $high) {
+        if ($nums[$mid] === 0) {
+            [$nums[$low], $nums[$mid]] = [$nums[$mid], $nums[$low]];
+            $low++;
+            $mid++;
+        } else if ($nums[$mid] === 1) {
+            $mid++;
+        } else {
+            [$nums[$mid], $nums[$high]] = [$nums[$high], $nums[$mid]];
+            $high--;
+        }
+    }
+}
+}
